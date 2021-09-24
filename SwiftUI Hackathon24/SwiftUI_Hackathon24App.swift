eae9a972063aa7952b1b90312957c98e0727f742
kirizat-theosophe <54266892+kirizat-theosophe@users.noreply.github.com>
2021-09-24 10:02:41 +0200

    Initial Commit

---
+++ SwiftUI Hackathon24/SwiftUI_Hackathon24App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Hackathon24App.swift
+//  SwiftUI Hackathon24
+//
+//  Created by Bruker on 24/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Hackathon24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
